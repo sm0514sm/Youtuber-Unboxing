2ce6260b461aebb2a676158322d029e842a78176
mpcato <mpcato@naver.com>
2020-01-28 09:43:36 +0900

    add comment to getLatestVideos

--- API_Source/getLatestVideos.py
+++ API_Source/getLatestVideos.py
@@ -5,11 +5,9 @@ from decouple import config
 # channeID로 최신 동영상 가져오기
 
 
-
-
 channelID = 'UCUpJs89fSBXNolQGOYKn0YQ'
 KEY = config('GOOGLEAPIKEY')
-maxResult = 2
+maxResult = 2 # 가져올 동영상의 수
 URL = 'https://www.googleapis.com/youtube/v3/search?key={}&channelId={}&part=snippet,id&order=date&maxResults={}'.format(KEY, channelID, maxResult)
 response = urlopen(URL).read().decode('utf8')
 # headers = {'Content-Type': 'charset=utf-8'}
