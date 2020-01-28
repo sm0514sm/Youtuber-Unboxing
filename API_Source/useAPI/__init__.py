b3f1fe40289deebce68a1270a49ad2f579bee1db
이상민 <sm0514sm@gmail.com>
2020-01-28 11:49:30 +0900

    add channel info

--- API_Source/useAPI/__init__.py
+++ API_Source/useAPI/__init__.py
@@ -1,7 +1,15 @@
 from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup as BS
 from decouple import config
 from urllib.request import urlopen, unquote
-from bs4 import BeautifulSoup as BS
+import urllib.request
+import json
+import sys
+import io
+
+############### print 할 때 utf-8 디코딩이 안 될 경우 주석 해제
+# sys.stdout = io.TextIOWrapper(sys.stdout.detach(), encoding = 'utf-8')
+# sys.stderr = io.TextIOWrapper(sys.stderr.detach(), encoding = 'utf-8')
 
 
 def get_channelID_from_URL(input_URL):
@@ -25,27 +33,77 @@ def get_latest_videos_using_channelID(channelID):
     response = urlopen(URL).read().decode('utf8')
     # response에 str 타입으로 자료가 넘어옴
 
+    print(type(response))
+    print(response)
+
+
+def get_video_details(videoID):
+    URL = 'https://www.googleapis.com/youtube/v3/videos?part=snippet&id={}&key={}'.format(videoID, config('GOOGLEAPIKEY'))
+    response = urlopen(URL).read().decode('utf-8')
+    res_dict = json.loads(response)
+    # print(response)
+    # 들어온 정보를 JSON으로 저장
+    PATH = 'zzzz.json'
+    with open("{}".format(PATH), 'w', encoding='utf-8-sig') as file:
+        json.dump(res_dict, file, indent="\t", ensure_ascii=False)
+
 
 def get_channel_other_sites(input_url):
+    other_link_list = []
     html = urlopen(input_url).read()
     soup = BS(html, "html.parser", from_encoding='utf-8')
-    ff = soup.find_all("button", attrs={'class': "yt-uix-button yt-uix-button-size-default "
-                                                 "yt-uix-button-subscribe-branded yt-uix-button-has-icon "
-                                                 "no-icon-markup yt-uix-subscription-button yt-can-buffer"})
-    youtube_id = ff[0].get('data-channel-external-id')
-    print(youtube_id)
-
     li_tags = soup.find_all("li", attrs={'class': "channel-links-item"})
     for li_tag in li_tags:
         before_link = li_tag.find('a').get('href')
         link = unquote(before_link[before_link.find('http'):])
         if '&' in link:
             link = link[:link.find('&')]
-        print(link)
+        other_link_list.append(link)
+    return other_link_list
 
 
-# response = requests.get(URL).text
+def get_channel_info(channelID):
+    channel_info_dict = {}
+    base_url = "https://www.googleapis.com/youtube/v3/channels"
+    part = "id,snippet,brandingSettings,contentDetails,invideoPromotion,statistics,topicDetails"
+    response = urllib.request.urlopen(base_url + "?part=%s&id=%s&key=%s" % (part, channelID, key))
+    string = response.read().decode('utf-8')
+    json_obj = json.loads(string)
+
+    snippet = json_obj["items"][0]["snippet"]
+    title = snippet["title"]
+    description = snippet["description"]
+    thumbnail = snippet["thumbnails"]["default"]["url"]
+    channel_info_dict["title"] = title
+    channel_info_dict["description"] = description
+    channel_info_dict["thumbnail"] = thumbnail
+
+    statistics = json_obj["items"][0]["statistics"]
+    view_count = statistics["viewCount"]
+    subscriber_count = statistics["subscriberCount"]
+    video_count = statistics["videoCount"]
+    channel_info_dict["viewCount"] = view_count
+    channel_info_dict["subscriberCount"] = subscriber_count
+    channel_info_dict["videoCount"] = video_count
+
+    return channel_info_dict
 
 
-print(type(response))
-print(response)
+if __name__ == "__main__":
+    key = config('GOOGLEAPIKEY')
+
+    url = "https://www.youtube.com/user/officialpsy"
+    channel_id = get_channelID_from_URL(url)
+    # get_latest_videos_using_channelID(channel_id)
+    other_links = get_channel_other_sites(url)
+    channel_info = get_channel_info(channel_id)
+
+
+    print('* url : ' + url)
+    print('* channel_id : ' + channel_id)
+    print('* other_links : ' + str(len(other_links)))
+    for index, link in enumerate(other_links):
+        print('\t%d - %s' %(index + 1, link))
+    print('* channel_info')
+    for item in channel_info:
+        print('\t%s - %s' %(item, channel_info[item]))
