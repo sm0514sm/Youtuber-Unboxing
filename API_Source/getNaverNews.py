03efdefa3bd6228ae8e0af756887586705fae0fa
이상민 <sm0514sm@gmail.com>
2020-01-29 16:39:52 +0900

    update get naver news

--- API_Source/getNaverNews.py
+++ API_Source/getNaverNews.py
@@ -5,7 +5,7 @@ import datetime
 
 # 기본, 게임, 엔터테인먼트, 뷰티, 스포츠, 먹망, 키즈, 동물, 일상, IT
 NECESSARY_WORD = [
-    ['유튜브', '유튜버', '유투버', '채널', '방송'],
+    ['유튜브', '유튜버', '유투버', '채널'],
     ['게임', 'game'],
     ['엔터테인먼트', '예능', 'entertainment'],
     ['뷰티', '화장', '패션'],
@@ -18,8 +18,8 @@ NECESSARY_WORD = [
 ]
 
 MONTH = ['', 'Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
-YOUTUBER = '워크맨'    # DB 에서 yno 에 해당하는 channelName or youtubeName 을 입력
-CATEGORY = [1, 2]     # 해당 유튜버의 카테고리가 게임과 엔터테인먼트
+YOUTUBER = 'workman'    # DB 에서 yno 에 해당하는 channelName or youtubeName 을 입력
+CATEGORY = [2]     # 해당 유튜버의 카테고리가 게임과 엔터테인먼트
 
 
 NEWS_URL = 'https://openapi.naver.com/v1/search/news.json?'
@@ -30,8 +30,8 @@ WEB_URL = 'https://openapi.naver.com/v1/search/webkr.json?'
 URL = NEWS_URL
 params = {
     'query': YOUTUBER,
-    'display': '100',
-    'start': '13',
+    'display': 100,
+    'start': 1,
     'sort': 'date'
 }
 
@@ -47,29 +47,34 @@ for num in CATEGORY:
 # 만약 last updatedDate 가 2019-03-15 면 3월 15일 이후의 기사만 가져와야함
 updatedDate = datetime.datetime(2019, 3, 15)
 
-response = requests.get(URL, params=params, headers=headers).text  # str type
-newses = json.loads(response)["items"]
+while True:
+    response = requests.get(URL, params=params, headers=headers).text  # str type
+    total = json.loads(response)["total"]
+    newses = json.loads(response)["items"]
 
-for (index, news) in enumerate(newses):
-    isCorrect = False
-    date = datetime.datetime(int(news["pubDate"][12:16]), MONTH.index(news["pubDate"][8:11]), int(news["pubDate"][5:7]))
+    for (index, news) in enumerate(newses):
+        isCorrect = False
+        date = datetime.datetime(int(news["pubDate"][12:16]), MONTH.index(news["pubDate"][8:11]), int(news["pubDate"][5:7]))
 
-    # 업데이트되어있는 기사는 추가하지 않음
-    if (date - updatedDate).days < 0:
-        break
-
-    # Description 에 특정 키워드가 없는 기사는 추가하지 않음
-    for keyword in category_keyword:
-        if keyword in news["description"]:
-            isCorrect = True
+        # 업데이트되어있는 기사는 추가하지 않음
+        if (date - updatedDate).days < 0:
             break
-    if not isCorrect:
-        continue
 
-    newNews = {
-        'newsLink': news["link"],
-        'newsTitle': news["title"],
-        'newsDescription': news["description"],
-        'newsDate': str(date)[:10]
-    }
-    print(newNews)
+        # Description 에 특정 키워드가 없는 기사는 추가하지 않음
+        for keyword in category_keyword:
+            if keyword in news["description"]:
+                isCorrect = True
+                break
+        if not isCorrect:
+            continue
+
+        newNews = {
+            'newsLink': news["link"],
+            'newsTitle': news["title"],
+            'newsDescription': news["description"],
+            'newsDate': str(date)[:10]
+        }
+        print(newNews)
+    if params['start'] >= min(1000, total):
+        break
+    params['start'] += params['display']
