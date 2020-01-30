a2b977457498c3cbde0bc76dab9d59195d73396d
김태민 <taeminkim23@gmail.com>
2020-01-30 15:33:49 +0900

    make youtuberPage_basicComponent

--- frontEnd/project_front/src/views/youtuberPage.vue
+++ frontEnd/project_front/src/views/youtuberPage.vue
@@ -1,19 +1,92 @@
 <template>
-  <!-- vuetify를 참고하여 작성하기
-  https://vuetifyjs.com/ko/components/api-explorer
-  -->
   <div>
-    <v-card>
+    <!-- header -->
+    <v-card flat class="pa-0">
+      <v-img :src="youtuber.bannerImageLink" class="py-6 lighten-5">
+        <!-- <i class="font-weight-black display-4 jb">YOUTUBER</i> -->
+      </v-img>
+    </v-card>
+
+    <!-- <v-card>
       <v-card-title class="justify-center py-6" style="background-color:#cdcdcd ; height : 300px">
         <i class="font-weight-black display-3" color="red">YOUTUBERPAGE</i>
       </v-card-title>
-    </v-card>
+    </v-card>-->
+
+    <!-- content -->
     <v-container>
-      <v-card flat>
+      <!-- <v-card flat class="pa-0">
         <v-img :src="youtuber.bannerImageLink" class="py-6 lighten-5">
-          <!-- <i class="font-weight-black display-4 jb">YOUTUBER</i> -->
         </v-img>
-      </v-card>
+      </v-card>-->
+      <transition appear name="slide-fade">
+        <!--기본정보-->
+        <v-card class="my-3" outlined flat>
+          <v-container fluid>
+            <v-row>
+              <!-- thumbnail -->
+              <v-col cols="2" class="pa-3">
+                <v-img class="circle" :src="youtuber.thumbnails" flat :aspect-ratio="1/1" />
+              </v-col>
+
+              <!-- 기본정보 -->
+              <v-col cols="10">
+                <v-row>
+                  <v-col class="pb-0">
+                    <p class="font-weight-black thin display-3 ma-0">{{youtuber.channelName}}</p>
+                  </v-col>
+                </v-row>
+                <v-row>
+                  <v-col class="pt-0">
+                    <span class="font-weight-light">개설일 : {{youtuber.publishedDate}} &nbsp;&nbsp;</span>
+                  </v-col>
+                </v-row>
+                <v-row>
+                  <v-col class="pa-0">
+                    <v-divider></v-divider>
+                  </v-col>
+                </v-row>
+
+                <v-row>
+                  <v-col>
+                    <span class="font-weight-bold">구독자 수</span>
+                    <br />
+                    {{youtuber.subscriber}}
+                  </v-col>
+                  <v-divider vertical class="mx-3"></v-divider>
+                  <v-col>
+                    <span class="font-weight-bold">총 영상 수</span>
+                    <br />
+                    {{youtuber.totalVideoCount}}
+                  </v-col>
+                  <v-divider vertical class="mx-3"></v-divider>
+                  <v-col>
+                    <span class="font-weight-bold">총 영상조회 수</span>
+                    <br />
+                    {{youtuber.totalViewCount}}
+                  </v-col>
+                  <v-spacer></v-spacer>
+                </v-row>
+              </v-col>
+            </v-row>
+          </v-container>
+        </v-card>
+      </transition>
+
+        <v-row>
+          <v-col cols="9">
+            <v-card outlined flat>sdfsdf</v-card>
+          </v-col>
+          <v-col cols="3">
+            <v-card outlined flat>
+              <v-avatar color="red">
+                <span class="white--text headline">A</span>
+              </v-avatar>
+            </v-card>
+          </v-col>
+        </v-row>
+
+      <!-- mainContainer -->
       {{this.$route.query.yno}}
       <v-divider></v-divider>
       {{youtuber.yno}}
@@ -69,4 +142,27 @@ export default {
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+.slide-fade-enter-active {
+  transition: all 2s ease;
+}
+.slide-fade-leave-active {
+  transition: all 0.8s cubic-bezier(1, 0.5, 0.8, 1);
+}
+.slide-fade-enter, .slide-fade-leave-to
+/* .slide-fade-leave-active below version 2.1.8 */ {
+  transform: translateX(100px);
+  opacity: 0;
+}
+.circle {
+  border-radius: 50%;
+}
+.jb {
+  text-align: center;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  color: red;
+}
+</style>
