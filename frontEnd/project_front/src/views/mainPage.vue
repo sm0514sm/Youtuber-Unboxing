93b9ebc1d7f43e2d2a2f349b7b424f0bc75322b8
김주희 <yylcd9999@naver.com>
2020-01-30 18:05:59 +0900

    edit vue main page design

--- frontEnd/project_front/src/views/mainPage.vue
+++ frontEnd/project_front/src/views/mainPage.vue
@@ -9,19 +9,20 @@
     </image-preloader>
         <v-card>
             <v-img :src="require('@/assets/coolcat.png')" class="py-6 lighten-5">
-                <i class="font-weight-black display-4 jb">CAT<br> DOMINATES<br>THE WORLD</i>
+                <i class="font-weight-black display-4 jb mb-5 pm-5">YOUTUBESWAG</i>
+                <!-- <v-text-field class="mt-5 jb" background-color="#be228a94" append-icon="mdi-magnify" flat hide-details solo-inverted style="max-width: 300px; " @keyup.enter="search" v-model="searchWord" :key="$route.fullPath"/> -->
             </v-img>
         </v-card>
     
-        <banner />
+        <!-- <banner /> -->
     
         <v-container transition="slide-y-transition">
-            <v-layout wrap>
-                <v-flex v-for="(category,i) in categories" :key=i xs6 md4 class="pa-3">
+            <v-layout wrap class="pa-12 ma-10">
+                <v-flex v-for="(category,i) in categories" :key=i xs6 md4 class="pa-8" background-color="none">
                     <v-responsive :aspect-ratio="1/1">
-                        <v-btn block :aspect-ratio="1/1" @click="onCategoryButtonClicked(i)" height="100%">
-                            <v-img :src="require('@/assets/logo.png')">
-                                <h1> {{category.nameEng}} </h1>
+                        <v-btn block :aspect-ratio="1/1" @click="onCategoryButtonClicked(i)" height="100%" width="50px" class="rounded-card">
+                            <v-img :src="require('@/assets/' + i + '.png')" class="rounded-card pa-5">
+                                <h1 font-family="PT sans"> {{category.nameEng}} </h1>
                             </v-img>
                         </v-btn>
                     </v-responsive>
@@ -37,14 +38,16 @@
 </template>
 
 <script>
-import banner from "../components/banner";
+// import banner from "../components/banner";
 import {
     mapGetters
 } from 'vuex'
 
 
 export default {
-    components: { banner },
+    components: { 
+        // banner 
+    },
     name: "mainPage",
     methods: {
         onCategoryButtonClicked(i) {
@@ -73,6 +76,13 @@ export default {
     top: 50%;
     left: 50%;
     transform: translate( -50%, -50%);
-    color: red
+    color: black;
+    background-color: #95b3e8a9;
+    margin-bottom: 50px;
+}
+.rounded-card{
+    
+    border-radius:150px;
+    box-shadow: none;
 }
 </style>
