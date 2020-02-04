cf5fd42d24a440f608c2de6a157888171cc4125f
김주희 <yylcd9999@naver.com>
2020-02-04 17:34:13 +0900

    add favorite rest

---
+++ spring-skeleton/src/main/java/com/ssafy/model/dao/FavoriteDao.java
@@ -0,0 +1,17 @@
+package com.ssafy.model.dao;
+
+import java.util.List;
+import java.util.Map;
+
+import org.apache.ibatis.annotations.Mapper;
+
+import com.ssafy.model.dto.Favorite;
+import com.ssafy.model.dto.Youtuber;
+
+@Mapper
+public interface FavoriteDao {
+	public void insertFavorite(Favorite favorite);
+	public void deleteFavorite(Map<String, Integer> condition);
+	public List<Youtuber> searchUserFavoriteYoutuber(int usno);
+	public int searchYoutuberFavoriteNum(int yno);
+}
---
+++ spring-skeleton/src/main/java/com/ssafy/model/service/FavoriteServiceImpl.java
@@ -0,0 +1,58 @@
+package com.ssafy.model.service;
+
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.ssafy.model.dao.FavoriteDao;
+import com.ssafy.model.dto.Favorite;
+import com.ssafy.model.dto.Youtuber;
+
+@Service
+public class FavoriteServiceImpl implements FavoriteService {
+	
+	@Autowired
+	private FavoriteDao dao;
+	
+	@Override
+	public void insertFavorite(Favorite favorite) {
+		try {
+			dao.insertFavorite(favorite);
+		} catch (Exception e) {
+			e.printStackTrace();
+			throw new RuntimeException("즐겨찾기 추가 중 에러가 발생했습니다.");
+		}
+	}
+	
+	@Override
+	public void deleteFavorite(Map<String, Integer> condition) {
+		try {
+			dao.deleteFavorite(condition);
+		} catch (Exception e) {
+			e.printStackTrace();
+			throw new RuntimeException("즐겨찾기 삭제 중 에러가 발생했습니다.");
+		}
+	}
+
+	@Override
+	public List<Youtuber> searchUserFavoriteYoutuber(int usno) {
+		try {
+			return dao.searchUserFavoriteYoutuber(usno);
+		} catch (Exception e) {
+			e.printStackTrace();
+			throw new RuntimeException("사용자가 즐겨찾기 한 유투버 목록 조회 중 에러가 발생했습니다.");
+		}
+	}
+	
+	@Override
+	public int searchYoutuberFavoriteNum(int yno) {
+		try {
+			return dao.searchYoutuberFavoriteNum(yno);
+		} catch (Exception e) {
+			e.printStackTrace();
+			throw new RuntimeException("유투버 고유번호로 해당 유투버를 즐겨찾기 한 사람 수 조회 중 에러가 발생했습니다.");
+		}
+	}
+}
