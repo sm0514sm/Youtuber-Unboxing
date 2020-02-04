ebc45afb58299e617771d209ab50702845b147ea
mpcato <mpcato@naver.com>
2020-02-04 17:47:46 +0900

    make daum Cafe API

--- API_Source/daum/daumCafe.py
+++ API_Source/daum/daumCafe.py
@@ -1,3 +1,4 @@
+ # -*- encoding: utf-8 -*-
 import urllib.request
 from decouple import config
 import json
@@ -12,8 +13,8 @@ youtuber = 7 # 유튜버 고유번호
 params = {
     'query': '피지컬갤러리',
     'sort': 'recency',
-    'page': '1',
-    'size': '10',
+    'page': 1,
+    'size': 30,
 }
 
 headers = {
@@ -24,36 +25,40 @@ URL = 'https://dapi.kakao.com/v2/search/cafe'
 
 response = requests.get(URL, headers=headers, params=params)
 res = json.loads(response.text)
-data = res.gt('documents')
 
-keywords = ['유튜브', '유튜버', '채널']
-
-new_rows = {
-    'yno': youtuber,
-    'articleTitle': '',
-    'articleLink': '',
-    'articleDescription': '',
-    'articleDate': '',
-    'communityName': '',
-}
+keywords = ['유튜브', '유튜버', '채널', '유튭']
 
 DB_list = []
-
-
+documents = res.get('documents')
 if (response.status_code == 200):
-    new_rows = {
-        'yno': youtuber,
-        'articleTitle': '',
-        'articleLink': '',
-        'articleDescription': '',
-        'articleDate': '',
-        'communityName': '',
-    }
-    # pprint(res)
+    for document in documents:
+        try:
+            contents = document.get('contents')
+            for keyword in keywords:
+                if keyword in contents:
+                    new_rows = {
+                        'yno': youtuber,
+                        'articleTitle': document.get('title'),
+                        'articleLink': document.get('url'),
+                        'articleDescription': document.get('contents'),
+                        'articleDate': document.get('datetime')[:10],
+                    }
+                    # pprint(new_rows)
+                    DB_list.append(new_rows)
+                    break
+        except:
+            print('error')
+            continue
+    # 날짜까지 계산
+    print(DB_list)
+    # PATH = 'daumCafeExample.json'
+    # with open("{}".format(PATH), 'w', encoding='utf-8-sig') as file:
+    #     json.dump(DB_list, file, indent="\t", ensure_ascii=False)
 else:
     print('error code : ' + response.status_code)
     
     
 # PATH = 'daumCafeExample.json'
-# with open("{}".format(PATH), 'w', encoding='utf-8-sig') as file: 
+# with open("{}".format(PATH), 'w', encoding='utf-8-sig') as file:
 #     json.dump(res, file, indent="\t", ensure_ascii=False)
+print('\U0001F61A')
