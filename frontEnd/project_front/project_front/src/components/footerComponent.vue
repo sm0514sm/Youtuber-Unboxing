d0c4c39e5beb3cc8ab1cf3c825a7aa7ba8bc93b8
이상민 <sm0514sm@gmail.com>
2020-02-10 16:47:56 +0900

    basci update footer

--- frontEnd/project_front/project_front/src/components/footerComponent.vue
+++ frontEnd/project_front/project_front/src/components/footerComponent.vue
@@ -1,5 +1,25 @@
 <template>
- <v-footer
+  <v-footer dark padless>
+    <v-card flat tile class="indigo lighten-1 white--text text-center">
+      <v-card-text>
+        <v-btn v-for="icon in icons" :key="icon" class="mx-4 white--text" icon>
+          <v-icon size="24px">{{ icon }}</v-icon>
+        </v-btn>
+      </v-card-text>
+
+      <v-card-text
+        class="white--text pt-0"
+      >Phasellus feugiat arcu sapien, et iaculis ipsum elementum sit amet. Mauris cursus commodo interdum. Praesent ut risus eget metus luctus accumsan id ultrices nunc. Sed at orci sed massa consectetur dignissim a sit amet dui. Duis commodo vitae velit et faucibus. Morbi vehicula lacinia malesuada. Nulla placerat augue vel ipsum ultrices, cursus iaculis dui sollicitudin. Vestibulum eu ipsum vel diam elementum tempor vel ut orci. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.</v-card-text>
+
+      <v-divider></v-divider>
+
+      <v-card-text class="white--text">
+        {{ new Date().getFullYear() }} —
+        <strong>Vuetify</strong>
+      </v-card-text>
+    </v-card>
+  </v-footer>
+  <!-- <v-footer
     class="py-4"
     dark
     height="auto"
@@ -20,46 +40,52 @@
         </v-btn>
       </v-layout>
     </v-container>
-  </v-footer>
+  </v-footer>-->
 </template>
 
 <script>
 export default {
-    data: () => ({
-      items: [
-        {
-          href: '#!',
-          icon: 'mdi-twitter'
-        },
-        {
-          href: '#!',
-          icon: 'mdi-instagram'
-        },
-        {
-          href: '#!',
-          icon: 'mdi-facebook'
-        },
-        {
-          href: '#!',
-          icon: 'mdi-google-plus'
-        },
-        {
-          href: '#!',
-          icon: 'mdi-reddit'
-        },
-        {
-          href: '#!',
-          icon: 'mdi-discord'
-        },
-        {
-          href: '#!',
-          icon: 'mdi-pinterest'
-        }
-      ]
-    })
-}
+  data: () => ({
+    icons: [
+      "fa-facebook",
+      "fa-twitter",
+      "fab fa-google-plus",
+      "fab fa-linkedin",
+      "fab fa-instagram"
+    ]
+    // items: [
+    //   {
+    //     href: "#!",
+    //     icon: "mdi-twitter"
+    //   },
+    //   {
+    //     href: "#!",
+    //     icon: "mdi-instagram"
+    //   },
+    //   {
+    //     href: "#!",
+    //     icon: "mdi-facebook"
+    //   },
+    //   {
+    //     href: "#!",
+    //     icon: "mdi-google-plus"
+    //   },
+    //   {
+    //     href: "#!",
+    //     icon: "mdi-reddit"
+    //   },
+    //   {
+    //     href: "#!",
+    //     icon: "mdi-discord"
+    //   },
+    //   {
+    //     href: "#!",
+    //     icon: "mdi-pinterest"
+    //   }
+    // ]
+  })
+};
 </script>
 
 <style scoped>
-
 </style>
