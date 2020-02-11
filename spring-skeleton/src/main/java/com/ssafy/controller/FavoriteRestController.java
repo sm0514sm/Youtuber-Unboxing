200b85301621214e5074f085df4e170e389b2c8d
김주희 <yylcd9999@naver.com>
2020-02-11 18:09:31 +0900

    edit favorite,user controller

--- spring-skeleton/src/main/java/com/ssafy/controller/FavoriteRestController.java
+++ spring-skeleton/src/main/java/com/ssafy/controller/FavoriteRestController.java
@@ -13,12 +13,18 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.google.gson.Gson;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParser;
 import com.ssafy.model.dto.Favorite;
+import com.ssafy.model.dto.User;
 import com.ssafy.model.dto.Youtuber;
 import com.ssafy.model.service.FavoriteService;
+import com.ssafy.model.service.KakaoAPI;
+import com.ssafy.model.service.UserService;
 
 import io.swagger.annotations.ApiOperation;
 
@@ -27,46 +33,75 @@ import io.swagger.annotations.ApiOperation;
 public class FavoriteRestController {
 	@Autowired
 	private FavoriteService favoriteService;
+	@Autowired
+	private KakaoAPI kakao;
+	@Autowired
+	private UserService userService;
 	
 	@ExceptionHandler
 	public ResponseEntity<Map<String, Object>> handle(Exception e){
 		return handleFail(e.getMessage(), HttpStatus.OK);
 	}
 	
-	@ApiOperation("youtuber 고유번호, user 고유번호 | 해당 youtuber를 해당 user의 즐겨찾기에 추가")
-	@PostMapping("/favorite/insert/{yno}_{usno}")
-	public ResponseEntity<Map<String, Object>> insertFavorite(@PathVariable int yno, @PathVariable int usno){
+	@ApiOperation("해당 youtuber를 해당 user의 즐겨찾기에 추가")
+	@PostMapping("/favorite/insert")
+	public ResponseEntity<Map<String, Object>> insertFavorite(@RequestParam Map<String, Object> fav){
+		Gson gson = new Gson(); 
+		String json = gson.toJson(fav); 
+    	JsonParser parser = new JsonParser();
+    	JsonElement element = parser.parse(json);
+    	
+    	HashMap<String, Object> userInfo = kakao.getUserInfo(element.getAsJsonObject().get("token").getAsString());
+    	User user = userService.search(userInfo.get("id").toString());
 		Favorite favorite = new Favorite();
-		favorite.setYno(yno);
-		favorite.setUsno(usno);
-		favoriteService.insertFavorite(favorite); 
+		favorite.setUsno(user.getUsno());
+		favorite.setYno(element.getAsJsonObject().get("yno").getAsInt());
+		
+		favoriteService.insertFavorite(favorite);
 		return handleSuccess("즐겨찾기 등록 완료");
 	}
 	
 	@ApiOperation("youtuber 고유번호, user 고유번호 | 해당 youtuber를 해당 user의 즐겨찾기에서 삭제")
-	@DeleteMapping("/favorite/delete/{yno}_{usno}")
-	public ResponseEntity<Map<String, Object>> searchKeyword(@PathVariable int yno,  @PathVariable int usno){
+	@DeleteMapping("/favorite/delete/{yno}&{usno}")
+	public ResponseEntity<Map<String, Object>> searchKeyword(@PathVariable String yno,  @PathVariable String usno){
+		System.out.println(yno);
+		System.out.println(usno);
+		HashMap<String, Object> userInfo = kakao.getUserInfo(usno.toString());
+    	User user = userService.search(userInfo.get("id").toString());
+		
 		Map<String, Integer> map = new HashMap<>();
-		map.put("yno", yno);
-		map.put("usno", usno);
+		map.put("yno", Integer.parseInt(yno));
+		map.put("usno", user.getUsno());
+		System.out.println("yno: "+yno);
+		System.out.println("usno: "+user.getUsno());
 		favoriteService.deleteFavorite(map); 
 		return handleSuccess("즐겨찾기 삭제 완료");
 	}
 	
 	@ApiOperation("youtuber 고유번호, user 고유번호 | 해당 user의 즐겨찾기에 해당 youtuber가 있는지 조회 (있으면 1, 없으면 0)")
-	@GetMapping("/favorite/select/{yno}_{usno}")
-	public ResponseEntity<Map<String, Object>> searchFavorite(@PathVariable int yno, @PathVariable int usno){
+	@GetMapping("/favorite/select/{yno}&{usno}")
+	public ResponseEntity<Map<String, Object>> searchFavorite(@PathVariable String yno, @PathVariable String usno){
+		System.out.println(yno);
+		System.out.println(usno);
+		HashMap<String, Object> userInfo = kakao.getUserInfo(usno.toString());
+    	User user = userService.search(userInfo.get("id").toString());
+		
 		Map<String, Integer> map = new HashMap<>();
-		map.put("yno", yno);
-		map.put("usno", usno);
+		map.put("yno", Integer.parseInt(yno));
+		map.put("usno", user.getUsno());
+		System.out.println("yno: "+yno);
+		System.out.println("usno: "+user.getUsno());
 		int count = favoriteService.searchFavorite(map);
 		return handleSuccess(count);
 	}
 	
 	@ApiOperation("user 고유번호 | 해당 user가 즐겨찾기 한  youtuber 목록 검색 | 즐겨찾기 추가 날짜 기준 내림차순")
 	@GetMapping("/favorite/user/{usno}")
-	public ResponseEntity<Map<String, Object>> searchUserFavoriteYoutuber(@PathVariable int usno){
-		List<Youtuber> list = favoriteService.searchUserFavoriteYoutuber(usno); 
+	public ResponseEntity<Map<String, Object>> searchUserFavoriteYoutuber(@PathVariable String usno){
+		System.out.println(usno);
+		HashMap<String, Object> userInfo = kakao.getUserInfo(usno.toString());
+    	User user = userService.search(userInfo.get("id").toString());
+		List<Youtuber> list = favoriteService.searchUserFavoriteYoutuber(user.getUsno()); 
 		return handleSuccess(list);
 	}
 	
@@ -90,4 +125,5 @@ public class FavoriteRestController {
 		resultMap.put("data", data);
 		return new ResponseEntity<Map<String,Object>>(resultMap, HttpStatus.OK);
 	}
+	
 }
---
+++ spring-skeleton/src/main/java/com/ssafy/model/dto/Interest.java
@@ -0,0 +1,28 @@
+package com.ssafy.model.dto;
+
+public class Interest {
+	//interest 테이블 + interest_user_relation 테이블의 컬럼을 합쳐놓은 DTO
+	private int itno;
+	private String itName;
+	
+	private int usno;		//interest_user_relation 테이블의 컬럼
+
+	public int getItno() {
+		return itno;
+	}
+	public void setItno(int itno) {
+		this.itno = itno;
+	}
+	public String getItName() {
+		return itName;
+	}
+	public void setItName(String itName) {
+		this.itName = itName;
+	}
+	public int getUsno() {
+		return usno;
+	}
+	public void setUsno(int usno) {
+		this.usno = usno;
+	}
+}
