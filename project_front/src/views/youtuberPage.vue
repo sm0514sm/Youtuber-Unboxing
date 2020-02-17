d235eddca17b86b33bce4a54341a11884e847168
이상민 <sm0514sm@gmail.com>
2020-02-17 17:28:45 +0900

    update bug fix

--- project_front/src/views/youtuberPage.vue
+++ project_front/src/views/youtuberPage.vue
@@ -47,6 +47,7 @@
                         color="blue"
                         @click="onCategoryButtonClicked(category.cano)"
                         justify="center"
+                        class="mr-1"
                       >
                         <b>{{category.name}}</b>
                       </v-btn>
@@ -650,6 +651,7 @@
                   dense
                   outlined
                   style="margin-left: auto; margin-right: auto;"
+                  v-else
                 >태그가 없습니다!</v-alert>
               </v-row>
             </v-card>
@@ -682,7 +684,7 @@
                   </v-card>
                 </v-hover>
               </v-row>
-              <v-row>
+              <v-row v-else>
                 <v-alert
                   type="info"
                   dense
