cd0753d9f1cc39d32e9114029f9378d88b1d56e8
김주희 <yylcd9999@naver.com>
2020-02-18 15:03:02 +0900

    edit mainpage detail css

--- project_front/src/views/mainPage.vue
+++ project_front/src/views/mainPage.vue
@@ -3,7 +3,6 @@
                       https://vuetifyjs.com/ko/components/api-explorer
   -->
   <div>
-    <!-- :src="require('@/assets/twice.webm')" -->
     <video
       :src="require('@/assets/bg' + bgNum + '.mp4')"
       class="bgvideo"
@@ -14,18 +13,13 @@
       muted
       style="margin: 0 auto; padding: 0;"
     ></video>
-    <!-- <image-preloader
-    :src="require('@/assets/coolcat.png')">
-    </image-preloader>-->
 
     <div class="websiteName">
       {{ typo }}
-      <div style="display: inline; color: rgba(255, 255, 255, 0.8)">
-        {{ cursor }}
-      </div>
+      <div style="display: inline; color: rgba(255, 255, 255, 0.8)">{{ cursor }}</div>
     </div>
 
-    <div style="position: absolute; top: 14.5%; left: 37%; width: 300px;">
+    <div class="searchForm">
       <v-autocomplete
         :items="searchItems"
         :search-input.sync="inputKeyword"
@@ -36,9 +30,8 @@
         ref="keyword"
         id="keyword"
         label="유튜버를 검색해보세요"
-        style="max-width: 300px; "
-        solo-inverted
-        flat
+        background-color="rgba(255, 255, 255, 0.95)"
+        height="50px"
       >
         <template v-slot:no-data>
           <v-list-item>
@@ -47,17 +40,12 @@
         </template>
 
         <template v-slot:item="{ item }">
-          <v-list-item-avatar
-            color="red"
-            class="headline font-weight-light white--text"
-          >
+          <v-list-item-avatar color="red" class="headline font-weight-light white--text">
             <img :src="item.thumbnails" alt="John" />
           </v-list-item-avatar>
-          <v-list-item-content>
+          <v-list-item-content style="width:100px;">
             <v-list-item-title v-text="item.channelName"></v-list-item-title>
-            <v-list-item-subtitle
-              >구독자 : {{ tc(item.subscriber) }}</v-list-item-subtitle
-            >
+            <v-list-item-subtitle>구독자 : {{ tc(item.subscriber) }}</v-list-item-subtitle>
           </v-list-item-content>
         </template>
       </v-autocomplete>
@@ -65,106 +53,24 @@
     </div>
     <div class="allCategory">
       <div class="categorySet">
-        <!-- <v-btn block :aspect-ratio="1/1" @click="onCategoryButtonClicked(0)" class="categorySet"> -->
         <v-icon
           @click="onCategoryButtonClicked(0)"
           class="material-icons"
           color="red"
-          >mdi-widgets-outline</v-icon
-        >
-        <!-- </v-btn> -->
+        >mdi-widgets-outline</v-icon>
         <div class="categoryName" @click="onCategoryButtonClicked(0)">전체</div>
       </div>
-      <div class="categorySet">
-        <v-icon
-          @click="onCategoryButtonClicked(1)"
-          class="material-icons"
-          color="pink"
-          >mdi-gamepad-variant</v-icon
-        >
-        <div class="categoryName" @click="onCategoryButtonClicked(1)">게임</div>
-      </div>
-      <div class="categorySet">
-        <v-icon
-          @click="onCategoryButtonClicked(2)"
-          class="material-icons"
-          color="purple"
-          >mdi-television-classic</v-icon
-        >
-        <div class="categoryName" @click="onCategoryButtonClicked(2)">엔터</div>
-      </div>
-      <div class="categorySet">
-        <v-icon
-          @click="onCategoryButtonClicked(3)"
-          class="material-icons"
-          color="deep-purple"
-          >mdi-palette</v-icon
-        >
-        <div class="categoryName" @click="onCategoryButtonClicked(3)">
-          스타일
-        </div>
-      </div>
-      <div class="categorySet">
-        <v-icon
-          @click="onCategoryButtonClicked(4)"
-          class="material-icons"
-          color="indigo"
-          >mdi-weight-lifter</v-icon
-        >
-        <div class="categoryName" @click="onCategoryButtonClicked(4)">운동</div>
-      </div>
-      <div class="categorySet">
-        <v-icon
-          @click="onCategoryButtonClicked(5)"
-          class="material-icons"
-          color="blue"
-          >mdi-food</v-icon
-        >
-        <div class="categoryName" @click="onCategoryButtonClicked(5)">음식</div>
-      </div>
-      <div class="categorySet">
-        <v-icon
-          @click="onCategoryButtonClicked(6)"
-          class="material-icons"
-          color="cyan"
-          >mdi-baby-face-outline</v-icon
-        >
-        <div class="categoryName" @click="onCategoryButtonClicked(6)">키즈</div>
-      </div>
-      <div class="categorySet">
-        <v-icon
-          @click="onCategoryButtonClicked(7)"
-          class="material-icons"
-          color="teal"
-          >mdi-dog</v-icon
-        >
-        <div class="categoryName" @click="onCategoryButtonClicked(7)">동물</div>
-      </div>
-      <div class="categorySet">
-        <v-icon
-          @click="onCategoryButtonClicked(8)"
-          class="material-icons"
-          color="green"
-          >mdi-camera-outline</v-icon
-        >
-        <div class="categoryName" @click="onCategoryButtonClicked(8)">일상</div>
-      </div>
-      <div class="categorySet">
-        <v-icon
-          @click="onCategoryButtonClicked(9)"
-          class="material-icons"
-          color="yellow"
-          >mdi-cellphone-iphone</v-icon
-        >
-        <div class="categoryName" @click="onCategoryButtonClicked(9)">기술</div>
+
+      <div
+        class="categorySet"
+        v-for="(item,index) in categories"
+        :key="index+1"
+        @click="onCategoryButtonClicked(index+1)"
+      >
+        <v-icon class="material-icons" :color="item.iconColor">{{item.icon}}</v-icon>
+        <div class="categoryName">{{item.iconName}}</div>
       </div>
     </div>
-
-    <!-- <v-img :src="require('@/assets/giphy2.gif')" width="100%"></v-img> -->
-
-    <!-- <div
-      style="margin: 0; padding: 0; background-color: antiquewhite; height: 30px;"
-    /> -->
     <br />
     <br />
 
@@ -173,7 +79,8 @@
         <h1 style="margin-top: 15%; font-weight: bold;">강력한 검색 기능</h1>
         <h3>
           원하는 유튜버를 검색해 볼 수 있습니다.
-          <br />만약 내가 찾는 유튜버가 없다면? <br />자유롭게 추가하세요
+          <br />만약 내가 찾는 유튜버가 없다면?
+          <br />자유롭게 추가하세요
           <br />
         </h3>
         <input-component></input-component>
@@ -195,12 +102,11 @@
         <v-img :src="require('@/assets/main2.gif')" width="500px"></v-img>
       </div>
       <div style="display: inline-block; margin: 0 auto;" width="45%">
-        <h1 style="margin-top: 15%; font-weight: bold;">
-          완벽한 비교하기 기능
-        </h1>
+        <h1 style="margin-top: 15%; font-weight: bold;">완벽한 비교하기 기능</h1>
         <h3>
           번거롭게 하나하나 비교할 필요 없이
-          <br />한 화면에서 두 유튜버를 비교할 수 <br />있습니다.
+          <br />한 화면에서 두 유튜버를 비교할 수
+          <br />있습니다.
         </h3>
       </div>
     </div>
@@ -213,12 +119,11 @@
 
     <div style="display: flex; width: 100%;">
       <div style="display: inline-block; margin: 0 auto;" width="45%">
-        <h1 style="margin-top: 15%; font-weight: bold;">
-          간편한 즐겨찾기 기능
-        </h1>
+        <h1 style="margin-top: 15%; font-weight: bold;">간편한 즐겨찾기 기능</h1>
         <h3>
           마음에 드는 유튜버가 있다면
-          <br />클릭 한 번으로 추가하고 <br />언제든지 모아보세요
+          <br />클릭 한 번으로 추가하고
+          <br />언제든지 모아보세요
         </h3>
       </div>
 
@@ -238,56 +143,17 @@
         <v-img :src="require('@/assets/7.png')" width="300px"></v-img>
       </div>
       <div style="display: inline-block; margin: 0 auto;" width="45%">
-        <h1 style="margin-top: 15%; font-weight: bold;">
-          완벽한 유튜버 추천 기능
-        </h1>
+        <h1 style="margin-top: 15%; font-weight: bold;">완벽한 유튜버 추천 기능</h1>
         <h3>
           나와 관심항목이 같은 사람들이
-          <br />즐겨찾는 유튜버를 추천해 드립니다 <br />바로 확인해 보세요
+          <br />즐겨찾는 유튜버를 추천해 드립니다
+          <br />바로 확인해 보세요
         </h3>
       </div>
     </div>
 
     <br />
     <br />
-    <!-- 
-        <v-container transition="slide-y-transition">
-            <v-layout wrap class="pa-12 ma-10">
-                <v-flex v-for="(category,i) in categories" :key=i xs6 md4 class="pa-8" background-color="none">
-                    <v-responsive :aspect-ratio="1/1">
-                        <v-btn block :aspect-ratio="1/1" @click="onCategoryButtonClicked(i)" height="100%" width="50px" class="rounded-card">
-                            <v-img :src="require('@/assets/' + i + '.png')" class="rounded-card pa-5">
-                                
-                            </v-img>
-                        </v-btn>
-                    </v-responsive>
-                    <h1 class="categoryName"> {{category.nameEng}} </h1>
-                </v-flex>
-            </v-layout>
-    </v-container>-->
-
-    <!-- <v-card>
-            <v-img :src="require('@/assets/coolcat.png')" class="py-6 lighten-5">
-                <i class="font-weight-black display-4 jb mb-5 pm-5">Youtube Analysis</i>
-            </v-img>
-    </v-card>-->
-
-    <!-- <banner /> -->
-
-    <!-- <v-container transition="slide-y-transition">
-            <v-layout wrap class="pa-12 ma-10">
-                <v-flex v-for="(category,i) in categories" :key=i xs6 md4 class="pa-8" background-color="none">
-                    <v-responsive :aspect-ratio="1/1">
-                        <v-btn block :aspect-ratio="1/1" @click="onCategoryButtonClicked(i)" height="100%" width="50px" class="rounded-card">
-                            <v-img :src="require('@/assets/' + i + '.png')" class="rounded-card pa-5">
-                                
-                            </v-img>
-                        </v-btn>
-                    </v-responsive>
-                    <h1 class="categoryName"> {{category.nameEng}} </h1>
-                </v-flex>
-            </v-layout>
-    </v-container>-->
   </div>
 </template>
 
@@ -346,6 +212,10 @@ export default {
       return tc(num);
     }
   },
+  mounted() {
+    document.getElementById("keyword").parentElement.style.marginLeft = "12px";
+    document.getElementById("keyword").parentElement.style.marginTop = "3px";
+  },
   computed: {
     ...mapGetters(["categories"]),
     ...mapGetters(["youtubersPerCategory"])
@@ -386,29 +256,15 @@ export default {
 
 <style scoped>
 @import url("https://fonts.googleapis.com/css?family=Do+Hyeon|Nanum+Gothic|Noto+Sans+KR&display=swap");
-.jb {
-  text-align: center;
-  position: absolute;
-  top: 50%;
-  left: 50%;
-  transform: translate(-50%, -50%);
-  color: black;
-  background-color: #95b3e8a9;
-  margin-bottom: 50px;
-}
 .rounded-card {
   border-radius: 20%;
   box-shadow: none;
 }
 .categoryName {
-  font-family: PT sans;
+  font-family: "Noto Sans KR", sans-serif;
   color: blacks;
   text-align: center;
 }
-/* .des_title {
-    font-size: 3.125rem;
-    margin-bottom: 0.5rem;
-} */
 .bgvideo {
   height: 100%;
   background-position: center;
@@ -418,19 +274,6 @@ export default {
 div {
   font-family: "Noto Sans KR", sans-serif;
 }
-/* .material-icons.md-18 { font-size: 18px; } */
-/* .material-icons.md-24 { font-size: 24px; } */
-
-/* .material-icons.md-48 { font-size: 48px; } */
-
-/* Rules for using icons as black on a light background. */
-/* .material-icons.md-dark { } */
-
-/* .material-icons.md-dark.md-inactive { color: rgba(0, 0, 0, 0.26); } */
-
-/* Rules for using icons as white on a dark background. */
-/* .material-icons.md-light { color: rgba(255, 255, 255, 1); } */
-/* .material-icons.md-light.md-inactive { color: rgba(255, 255, 255, 0.3); } */
 .allCategory {
   position: absolute;
   top: 20%;
@@ -438,7 +281,7 @@ div {
   transform: translateX(-50%);
   width: 780px;
   background-color: rgba(0, 0, 0, 0.2);
-  border-radius: 5%;
+  border-radius: 20px;
   /* background-color: #e3dada66; */
 }
 .material-icons {
@@ -461,15 +304,28 @@ div {
   padding: 0px 2px;
   width: 66px;
   color: white;
-  text-shadow: rgb(0, 0, 0) 0px 0px 2px;
+  text-shadow: rgb(0, 0, 0) 0px 0px 10px;
 }
 .websiteName {
   position: absolute;
-  top: 10%;
-  left: 33%;
+  top: 10.5%;
+  left: 32%;
   font-size: 56px;
   font-weight: 400;
   color: white;
-  text-shadow: rgb(0, 0, 0) 0px 0px 2px;
+  text-shadow: rgb(0, 0, 0) 0px 0px 7px;
+}
+.searchForm {
+  position: absolute;
+  top: 14%;
+  left: 50%;
+  transform: translateX(-50%);
+  width: 400px;
+}
+.searchForm div {
+  border-radius: 5px 5px 0px 0px;
+}
+.v-label {
+  padding-left: 50px;
 }
 </style>
