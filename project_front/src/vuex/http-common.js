25b24619d4d8a81112a5d4e27979859acc114c5b
sm0514sm <45008174+sm0514sm@users.noreply.github.com>
2020-08-23 23:18:02 +0900

    front에서 back ip 변경

--- project_front/src/vuex/http-common.js
+++ project_front/src/vuex/http-common.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-    baseURL: "http://15.165.77.1:8080/SpringBoot",
+    baseURL: "http://localhost:8080/SpringBoot",
     headers: {
         "Content-type": "application/json",
         'Access-Control-Allow-Origin': '*',
